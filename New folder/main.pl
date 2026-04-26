289a40ac91c15d9a5f702c10fcb2f54e342af60b
Heshan S. Pathum <heshanpathum19.official@gmail.com>
2026-04-26 22:23:06 +0530

    Update main.pl

--- New folder/main.pl
+++ New folder/main.pl
@@ -1,12 +1,12 @@
-%   1. Dynamic Predicates
+%    Dynamic Predicates
 :- dynamic current_vehicle/1.
 :- dynamic current_type/1.
 :- dynamic current_license/1.
 :- dynamic driver_credits/1.
 
-%  2. Static Facts 
+               %Facts 
 
-% Pre-stored 5 license numbers (8-digits each)
+% Pre stored 5 license numbers (8 digits)
 valid_license(10000000).
 valid_license(10000001).
 valid_license(10000002).
@@ -19,21 +19,21 @@ vehicle_multiplier(bus, 1.5).
 vehicle_multiplier(lorry, 2).
 vehicle_multiplier(bike, 0.5).
 
-% Violations: violation(Name, Base_Fine, Severity, Credit_Deduction)
+% Violations - violation(Name, Base_Fine, Severity, Credit_Deduction)
 violation(speeding, 3000, low, 2).
 violation(red_light, 5000, medium, 4).
 violation(drunk_driving, 25000, high, 10).
 violation(no_helmet, 2000, low, 1).
-
-%  3. Core Logic 
+     
+%   Core Logic 
 
 check_license(L_Num) :-
-    repeat,
-    write('Enter License Number (8 digits, e.g. 10023456.): '), 
+   repeat,
+    write('Enter License Number (8 digits, e.g. 10234567.): '), 
     read(L_Num),
     (   valid_license(L_Num) ->
         write('License verified. Valid License in Sri Lanka.'), nl, !
-    ;   nl,
+    ; nl,
         write('Error: Incorrect License Number!'), nl,
         write('Invalid Input: '), write(L_Num), nl,
         write('(Must be a registered 8-digit number)'), nl, nl,
@@ -46,7 +46,7 @@ check_status(Credits) :-
     write('Status: License is VALID.'), nl,
     write('Remaining Credit Balance: '), write(Credits), write(' credits.'), nl.
 check_status(_) :-
-    write('Status: License is now INVALID (Credits are over)!'), nl.
+   write('Status: License is now INVALID (Credits are over)!'), nl.
 
 %   Main Program 
 
@@ -66,7 +66,7 @@ start :-
     check_license(L_Num),
     assertz(current_license(L_Num)),
     
-    write('Enter Vehicle Number (e.g. wp_cad_1234.): '), read(V_Num),
+    write('Enter Vehicle Number (e.g. wp_cab_1234.): '), read(V_Num),
     write('Enter Vehicle Type (bike, car, bus, lorry.): '), read(V_Type),
     
     assertz(current_vehicle(V_Num)),
@@ -81,15 +81,15 @@ show_violations :-
     write('- drunk_driving'), nl,
     write('- no_helmet'), nl,
     
-    write('Enter the Violation Name : '), read(Offense),
+      write('Enter the Violation Name : '), read(Offense),
     generate_report(Offense).
 
 % checking no_helmet violation (its only for bike)
 generate_report(no_helmet) :-
     current_type(V_Type),
     V_Type \== bike, 
-    write('Error: "no_helmet" violation is ONLY for bikes!'), nl,
-    show_violations. % (Recursion)
+    write('Error: "no_helmet" violation is Only for bikes!'), nl,
+     show_violations. % (Recursion)
 
 generate_report(Offense) :-
     (violation(Offense, BaseFine, Severity, Penalty) ->
@@ -99,7 +99,7 @@ generate_report(Offense) :-
         % Calculate total fine based on vehicle type
         TotalFine is BaseFine * Multiplier,
         
-        % Credit deduction logic
+        % Credit deduction 
         driver_credits(Current),
         NewBalance is Current - Penalty,
         retract(driver_credits(Current)),
@@ -116,6 +116,7 @@ generate_report(Offense) :-
         write('Credit Deducted: '), write(Penalty), nl,
         check_status(NewBalance),
         write('========================================'), nl
-    ;
-        write('Invalid violation name. Please re-enter.'), nl, show_violations
+      ;
+        write('Invalid violation name. Please re-enter.'), nl, 
+          show_violations
     ).
